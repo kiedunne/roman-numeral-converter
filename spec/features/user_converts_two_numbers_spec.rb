69454aea96ebd37549c9ee12f4c8a911a0188a9c
kiedunne <kie.dunne@gmail.com>
2019-01-03 23:34:25 -0500

    user can convert many numbers

---
+++ spec/features/user_converts_two_numbers_spec.rb
@@ -0,0 +1,13 @@
+
+feature 'User converts two numbers' do
+  scenario 'User converts two numbers' do
+    visit('/')
+    fill_in :number, with: 3
+    click_on "convert"
+    expect(page).to have_content "III"
+    fill_in :number, with: 10
+    click_on "convert again"
+    expect(page).to have_content "X"
+    expect(page).not_to have_content "III"
+  end
+end
