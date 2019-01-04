23c33055cd77e4fd0a9bfdddbd9cfff21081444d
kiedunne <kie.dunne@gmail.com>
2019-01-03 20:38:09 -0500

    1 to I test passed

--- model/roman_numeral.rb
+++ model/roman_numeral.rb
@@ -1,5 +1,24 @@
+
 class RomanNumeral
 
-  def convert(number)
+  NUMERALS = {
+      100 => 'C',
+      90 => 'XC',
+      50 => 'L',
+      40 => 'XL',
+      10 => 'X',
+      5 => 'V',
+      4 => 'IV',
+      1 => 'I'
+    }
+
+  def initialize
+      @roman_numeral = []
   end
+
+  def convert(number)
+    NUMERALS.each { |key, val| (number/key).times { @roman_numeral << val ; number -= key } }
+    @roman_numeral.join
+  end
+
 end
