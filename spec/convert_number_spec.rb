418cf0a99c1dbb75839b5f9e0bb74613c0437f12
kiedunne <kie.dunne@gmail.com>
2019-01-03 20:44:32 -0500

    added more convert tests

--- spec/convert_number_spec.rb
+++ spec/convert_number_spec.rb
@@ -8,6 +8,18 @@ describe RomanNumeral do
     it 'changes 1 to I' do
       expect(subject.convert(1)).to eq 'I'
     end
+
+    it 'changes 3 to III' do
+      expect(subject.convert(3)).to eq 'III'
+    end
+
+    it 'changes 5 to V' do
+      expect(subject.convert(5)).to eq 'V'
+    end
+
+    it 'changes 10 to X' do
+      expect(subject.convert(10)).to eq 'X'
+    end
   end
 
 end
