5bae696d218ae64b15460543424bc90dadd6b5dc
kiedunne <kie.dunne@gmail.com>
2019-01-03 22:26:09 -0500

    user can enter a number feature

--- app.rb
+++ app.rb
@@ -4,6 +4,22 @@ require 'rspec'
 require_relative './model/roman_numeral.rb'
 
 class Converter < Sinatra::Base
+  enable :sessions
+  set :sessions_secret, 'My session secret'
+
+  get '/' do
+    erb(:index)
+  end
+
+  post '/convert' do
+    session[:number] = params[:number]
+    redirect '/result'
+  end
+
+  get '/result' do
+    @number = session[:number]
+    erb(:result)
+  end
 
   run! if app_file == $0
 end
--- spec/features/user_converts_number_spec.rb
+++
@@ -1,9 +0,0 @@
-
-feature 'Decimal number is converted to roman numeral' do
-  scenario 'User can see the converted numeral' do
-    visit('/')
-    fill_in :enter_num, with: 3
-    click_on "Convert"
-    expect(page).to have_content "III"
-  end
-end
