dbe8e6ac7aaa1d0129058f8a232b4b2973bc6005
kiedunne <kie.dunne@gmail.com>
2019-01-03 23:23:37 -0500

    user can see convert again button

---
+++ spec/features/user_converts_another_number_spec.rb
@@ -0,0 +1,9 @@
+
+feature 'User can convert another number' do
+  scenario 'User can see button to convert another number' do
+    visit('/')
+    fill_in :number, with: 3
+    click_on "convert"
+    expect(page).to have_button "convert again"
+  end
+end
