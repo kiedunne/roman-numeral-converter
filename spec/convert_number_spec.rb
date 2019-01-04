dd2b21753ad05b5ae53bc39e0b67bde8e068ed0e
kiedunne <kie.dunne@gmail.com>
2019-01-03 20:52:37 -0500

    added higher number convert tests

--- spec/convert_number_spec.rb
+++ spec/convert_number_spec.rb
@@ -13,12 +13,27 @@ describe RomanNumeral do
       expect(subject.convert(3)).to eq 'III'
     end
 
+    it 'changes 4 to IV' do
+      expect(subject.convert(4)).to eq 'IV'
+    end
     it 'changes 5 to V' do
       expect(subject.convert(5)).to eq 'V'
     end
 
     it 'changes 10 to X' do
       expect(subject.convert(10)).to eq 'X'
+    end
+
+    it 'changes 52 to LII' do
+      expect(subject.convert(52)).to eq 'LII'
+    end
+
+    it 'changes 86 to LXXXVI' do
+      expect(subject.convert(52)).to eq 'LII'
+    end
+
+    it 'changes 100 to C' do
+      expect(subject.convert(100)).to eq 'C'
     end
   end
 
